30ea1b3b56c38f5cbff62627fd008dfac112f275
Xenonheat <femiadesina@live.co.uk>
2019-01-08 12:50:52 +0000

    Successfully got ping to the url bypassing cloudflare

--- src/js/scraper.js
+++ src/js/scraper.js
@@ -7,38 +7,13 @@ const urlPrefix = 'https://kissanime.ac/Anime/';
 let kissanimeQuery = 'Baby-Steps';
 let cf = new CloudflareBypasser();
 
-cf.request('https://kissanime.ac/Anime/Baby-Steps', {
-  delay: 4000
-})
-.then(res => {
-  console.log('RES: ', res)
-});
-
-// const callRequest = (query) => {
-//   const url = urlPrefix.concat('', query);
-//   console.log('URL: ', url);
-//   cf.request({
-//     url: url,
-//     delay: 5000,
-//   })
-//   .then(html) => {
-//     console.log('HTML: ', html)
-//   }
-//   .catch((error) => {
-//     console.log('Experienced error while getting html: ', error)
-//   })
-// }
-
-// const callRequest = (query) => {
-//   const url = urlPrefix.concat('', query)
-//   console.log('URL: ', url)
-//   requestp(url).then((html) => {
-//     console.log('HTML: ', html)
-//   })
-//   .catch((error) => {
-//     console.log('Experienced error while getting html: ', error)
-//   })
-// }
-
-// callRequest(kissanimeQuery)
-
+try {
+  cf.request('https://kissanime.ac/Anime/Baby-Steps', {
+    delay: 6000
+  })
+  .then(res => {
+    console.log('RES: ', res)
+  });
+} catch(error) {
+  console.log('Experienced error whilst trying to access URL')
+}
